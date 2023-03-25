87b5d4ec530f19e1d6dd4227606ce0ed952e99cd
Alister Porteous <alisterporteous@hotmail.com>
2023-03-25 19:44:03 -0400

    removed unnecessary files from boiler code, added routes in App.js, added pages folder for home, about, projects, contact and resume

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="about" element={<h1>About</h1>} />
+          <Route path = "projects" element={<h1>Projects</h1>} />
+          <Route path = "contact" element={<h1>Contact</h1>} />
+          <Route path = "resume" element={<h1>Resume</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
