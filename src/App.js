b62a132917e1cb566c8773ce4748bf5c9d266c43
Alister Porteous <alisterporteous@hotmail.com>
2023-03-27 17:52:26 -0400

    installed react app, Material UI, Typist, Vertical Timeline, Formik, Yup and email js. imported pages and routes into app js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+ import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import Experience from './pages/Experience';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="about" element={<h1>About</h1>} />
-          <Route path = "projects" element={<h1>Projects</h1>} />
-          <Route path = "contact" element={<h1>Contact</h1>} />
-          <Route path = "resume" element={<h1>Resume</h1>} />
+          <Route path="/alisters-react-portfolio" element={<Home/>} />
+          <Route path="about" element={<About/>} />
+          <Route path = "projects" element={<Projects/>} />
+          <Route path = "contact" element={<Contact/>} />
+          <Route path = "experience" element={<Experience/>} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
