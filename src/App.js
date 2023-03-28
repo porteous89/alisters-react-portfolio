0c22bb7d42fc71a80fd56700624afee217054a5d
Alister Porteous <alisterporteous@hotmail.com>
2023-03-27 20:02:20 -0400

    deployed app properly, changed home file path

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/alisters-react-portfolio" element={<Home/>} />
+          <Route path="/" element={<Home/>} />
           <Route path="about" element={<About/>} />
           <Route path = "projects" element={<Projects/>} />
           <Route path = "contact" element={<Contact/>} />
